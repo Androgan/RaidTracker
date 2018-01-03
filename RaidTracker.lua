be684061b17e8cdf306b46db6f66a9bf0ce7cecc
Androgan <32416270+Androgan@users.noreply.github.com>
2018-01-03 15:42:21 +0100

    wip

--- RaidTracker.lua
+++ RaidTracker.lua
@@ -1,19 +1,32 @@
-local track = false
+-- local track = false
+-- local trackedZones = {"Molten Core", "Blackwing Lair"}
 
 function RaidTracker_OnLoad()
-	this:RegisterEvent("ZONE_CHANGED_INDOORS");
+	-- this:RegisterEvent("ZONE_CHANGED_INDOORS");
+  DEFAULT_CHAT_FRAME:AddMessage("Foo");
 end
 
-function RaidTracker_OnEvent()
-local zoneName
-  if(event == "ZONE_CHANGED_INDOORS") then
-    zoneName = GetZoneText();
-    if(zoneName in trackedZones) then
-      track = true
-    end
-  end
-end
+-- function RaidTracker_OnEvent()
+--   local zoneName
+-- 
+--   if(event == "ZONE_CHANGED_INDOORS") then
+--     zoneName = GetZoneText()
+--     if(searchZone(zoneName)) then
+--       track = true
+--     end
+--   end
+-- end
+-- 
+-- function trackAttendance()
+--   
+--   -- onGrpSizeChange if track do track
+-- end
 
-function trackAttendance()
-  -- onGrpSizeChange if track do track
-end
+-- function searchZone(zoneName)
+--   for _, v in pairs(trackedZones) do
+--     if(v == zoneName) then
+--       return true
+--     end
+--   end
+--   return false
+-- end
