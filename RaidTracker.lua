17e334ae04fb3bd8e6e652ae9a90a37eb63989e9
Androgan <32416270+Androgan@users.noreply.github.com>
2018-01-03 16:37:59 +0100

    wip

--- RaidTracker.lua
+++ RaidTracker.lua
@@ -1,32 +1,47 @@
--- local track = false
--- local trackedZones = {"Molten Core", "Blackwing Lair"}
+local track = false
+local trackedZones = {"Molten Core", "Blackwing Lair", "Ironforge"}
 
-function RaidTracker_OnLoad()
-	-- this:RegisterEvent("ZONE_CHANGED_INDOORS");
-  DEFAULT_CHAT_FRAME:AddMessage("Foo");
+local frame = CreateFrame("FRAME", "RaidTrackerFrame");
+frame:RegisterEvent("ZONE_CHANGED_INDOORS");
+frame:RegisterEvent("ZONE_CHANGED");
+frame:RegisterEvent("ZONE_CHANGED_NEW_AREA");
+frame:RegisterEvent("RAID_ROSTER_UPDATE");
+
+function eventHandler(self, event, ...)
+  if(event == "ZONE_CHANGED_NEW_AREA") then
+    myPrint("0");
+    -- checkTracking();
+    myPrint("0.5");
+  end
+  
+  if (event == "RAID_ROSTER_UPDATE") then
+    myPrint("1");
+    trackAttendance();
+  end
+end
+frame:SetScript("OnEvent", eventHandler);
+
+-- ----------------------------------------------------------------------------
+
+function trackAttendance()
+  if(track) then
+    myPrint("2");
+  end
 end
 
--- function RaidTracker_OnEvent()
---   local zoneName
--- 
---   if(event == "ZONE_CHANGED_INDOORS") then
---     zoneName = GetZoneText()
---     if(searchZone(zoneName)) then
---       track = true
---     end
---   end
--- end
--- 
--- function trackAttendance()
---   
---   -- onGrpSizeChange if track do track
--- end
+function checkTracking()
+  track = searchZone(GetZoneName());
+end
 
--- function searchZone(zoneName)
---   for _, v in pairs(trackedZones) do
---     if(v == zoneName) then
---       return true
---     end
---   end
---   return false
--- end
+function searchZone(zoneName)
+  for _, v in pairs(trackedZones) do
+    if(v == zoneName) then
+      return true
+    end
+  end
+  return false
+end
+
+function myPrint(text)
+  DEFAULT_CHAT_FRAME:AddMessage("RaidTracker: " ..text);
+end
